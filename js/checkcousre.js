adc392db0643f4096146773cd2f63cedec4abc9b
deepvn98 <deepvn988@gmail.com>
2021-07-10 09:13:23 +0700

    theem chi tieets khoas hocj

--- js/checkcousre.js
+++ js/checkcousre.js
@@ -1,21 +1,24 @@
-// window.onload = function (){
-//     getCourse();
-// }
-//
-// function getCourse(){
-//     $.ajax({
-//         type: "GET",
-//         url: "http://localhost:8080/api/course",
-//         success: function (data) {
-//             let c = `<div id="courseList">`;
-//             for (let i = 0; i <data.length ; i++) {
-//                 c+=`<div class="N-block"> <span class="d-left">Aug<br>
-//                                     26
-//                                     </span> <font color="#000" size="+1"><a href="#">${data[i].description}</a></font> </div>
-//                                     <hr>`;
-//
-//             }
-//             c+=`</div>`;
-//             document.getElementById("courseList").innerHTML=c;
-//     });
-// }
+window.onload = function (){
+    getCourse();
+}
+
+function getCourse() {
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/api/course",
+        success: function (data) {
+            let c = `<div id="courseList">`;
+            for (let i = 0; i < data.length; i++) {
+                c += `<div class="N-block"> <span class="d-left">District<br>
+                                    ${data[i].district.name}
+                                    </span> <font color="#000" size="+1"><a href="chitietkhoahoc.html">${data[i].description}</a>
+                                    </font> </div>
+                                    <hr>`;
+                // <a href="chitietkhoahoc.html">${data[i].description}</a>
+            }
+            c += `</div>`
+            document.getElementById("courseList").innerHTML = c;
+        }
+    });
+}
+
