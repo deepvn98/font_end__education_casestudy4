c2fad05c0687010d6a1a4137881f9b3ce5af6249
deepvn98 <deepvn988@gmail.com>
2021-07-10 15:14:16 +0700

    hiển thị các môn học"

--- js/showsubject.js
+++ js/showsubject.js
@@ -7,12 +7,12 @@ function getSubject() {
         type: "GET",
         url: "http://localhost:8080/api/subject",
         success: function (data) {
-            let c = `<div class="list_subject"> `;
+            let c = `<div id="list_subject">`;
+            c += `<select name="subject" id="subject-class">`;
             for (let i = 0; i < data.length; i++) {
-                c += `<select name="subject_class" id="subject-class">
-                                <option value="${data[i].id}">${data[i].name}</option>
-                            </select>`;
+                c +=`<option value="${data[i].id}">${data[i].name}</option>`;
             }
+            c+=`</select>`;
             c += `</div>`;
             document.getElementById("list_subject").innerHTML = c;
         }
