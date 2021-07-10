56b24ffb2aee90b8f10617dabf9533e48c688e29
hoang-gtvt <hoang92.gtvt@gmail.com>
2021-07-10 11:55:58 +0700

    sua lai method create

--- Js2/newUser.js
+++ Js2/newUser.js
@@ -3,13 +3,12 @@ function changeViewSubject(){
     $.ajax({
         type:"GET",
 
-
         url: "http://localhost:8080/api/subject",
         success: function (subjectList){
             console.log("in ra mang mang ", subjectList )
 
             let c="";
-            c+= '<select className="form-control"  id="user_subject" name="subject" multiple>'
+            c+= '<select className="form-control"  id="user_subject" name="subjects" multiple>'
                  for (let i = 0; i < subjectList.length; i++) {
 
                 c+= `<option value="${subjectList[i].id}">${subjectList[i].name}</option>`
@@ -22,6 +21,7 @@ function changeViewSubject(){
 }
 
 window.onload = function () {
+
     changeViewSubject();
 }
 
@@ -36,27 +36,50 @@ function teacherView(){
 }
 
 function addUser(){
-    let role= $('.role_user').val();
-    console.log(role);
-    let name= $('#user_name').val();
-    let username=$('#user_username').val();
-    let password=$('#user_password').val();
-    let email=$('#user_email').val();
-    let phone=$('#user_phone').val();
-    let address=$('#user_addr').val();
-    let description=$('#user_des').val();
-    let certificate=$('#user_cer').val();
+    // let role= $('.role_user').val();
+    // console.log(role);
+    // let name= $('#user_name').val();
+    // let username=$('#user_username').val();
+    // let password=$('#user_password').val();
+    // let email=$('#user_email').val();
+    // let phone=$('#user_phone').val();
+    // let address=$('#user_addr').val();
+    // let description=$('#user_des').val();
+    // let certificate=$('#user_cer').val();
+    //
+    // let subjects=$('#user_subject').val();
 
-    let subjects=$('#user_subject').val();
-
-    let form=$('#newUserForm')[10];
+    let form=$('#newUserForm')[0];
     let data = new FormData(form);
-
-    data.append("message","tap doi tuong thanh cong");
 
     // disabled the submit button
     // $("#btnSubmit").prop("disabled", true);
+    $.ajax({
+        type: "POST",
+        enctype: 'multipart/form-data',
+        url: "http://localhost:8080/api/user/create",
+        data: data,
+        processData: false,
+        contentType: false,
+        cache: false,
+        timeout: 600000,
+        success: function (data) {
+            alert(data);
 
-    console.log(data)
+        },
+    });
+
+}
+
+function checkUser(){
+    let username= localStorage.getItem("role");
+    if (username===""){
+
+        // $('#myModal').on('shown.bs.modal', function () {
+        //     $('#myInput').trigger('focus')
+        // })
+
+
+    }
 
 }
